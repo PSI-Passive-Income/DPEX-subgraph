f6e80a08242d3aeaaf9baca687b960345df9616a
Henk ter Harmsel <henkterharmsel92@gmail.com>
2021-05-03 16:57:00 +0200

    Fixed pasi dex subgraph

--- src/exchange/dayUpdates.ts
+++ src/exchange/dayUpdates.ts
@@ -1,114 +1,122 @@
+import { PairHourData } from '../../generated/schema'
 /* eslint-disable prefer-const */
-import { PairHourData } from "../../generated/schema";
-import { BigInt, BigDecimal, ethereum } from "@graphprotocol/graph-ts";
-import { Pair, Bundle, Token, DPexFactory, DPexDayData, PairDayData, TokenDayData } from "../../generated/schema";
-import { ONE_BI, ZERO_BD, ZERO_BI, FACTORY_ADDRESS } from "./utils";
+import { BigInt, BigDecimal, ethereum } from '@graphprotocol/graph-ts'
+import { Pair, Bundle, Token, DPexFactory, DPexDayData, PairDayData, TokenDayData } from '../../generated/schema'
+import { ONE_BI, ZERO_BD, ZERO_BI, FACTORY_ADDRESS } from './utils'
 
 export function updateDPexDayData(event: ethereum.Event): DPexDayData {
-  let pancake = DPexFactory.load(FACTORY_ADDRESS);
-  let timestamp = event.block.timestamp.toI32();
-  let dayID = timestamp / 86400;
-  let dayStartTimestamp = dayID * 86400;
-  let dpexDayData = DPexDayData.load(dayID.toString());
+  let dpex = DPexFactory.load(FACTORY_ADDRESS)
+  let timestamp = event.block.timestamp.toI32()
+  let dayID = timestamp / 86400
+  let dayStartTimestamp = dayID * 86400
+  let dpexDayData = DPexDayData.load(dayID.toString())
   if (dpexDayData === null) {
-    dpexDayData = new DPexDayData(dayID.toString());
-    dpexDayData.date = dayStartTimestamp;
-    dpexDayData.dailyVolumeUSD = ZERO_BD;
-    dpexDayData.dailyVolumeBNB = ZERO_BD;
-    dpexDayData.totalVolumeUSD = ZERO_BD;
-    dpexDayData.totalVolumeBNB = ZERO_BD;
-    dpexDayData.dailyVolumeUntracked = ZERO_BD;
+    dpexDayData = new DPexDayData(dayID.toString())
+    dpexDayData.date = dayStartTimestamp
+    dpexDayData.dailyVolumeUSD = ZERO_BD
+    dpexDayData.dailyVolumeBNB = ZERO_BD
+    dpexDayData.totalVolumeUSD = ZERO_BD
+    dpexDayData.totalVolumeBNB = ZERO_BD
+    dpexDayData.dailyVolumeUntracked = ZERO_BD
   }
 
-  dpexDayData.totalLiquidityUSD = pancake.totalLiquidityUSD;
-  dpexDayData.totalLiquidityBNB = pancake.totalLiquidityBNB;
-  dpexDayData.txCount = pancake.txCount;
-  dpexDayData.save();
+  dpexDayData.totalLiquidityUSD = dpex.totalLiquidityUSD
+  dpexDayData.totalLiquidityBNB = dpex.totalLiquidityBNB
+  dpexDayData.txCount = dpex.txCount
+  dpexDayData.save()
 
-  return dpexDayData as DPexDayData;
+  return dpexDayData as DPexDayData
 }
 
 export function updatePairDayData(event: ethereum.Event): PairDayData {
-  let timestamp = event.block.timestamp.toI32();
-  let dayID = timestamp / 86400;
-  let dayStartTimestamp = dayID * 86400;
-  let dayPairID = event.address.toHexString().concat("-").concat(BigInt.fromI32(dayID).toString());
-  let pair = Pair.load(event.address.toHexString());
-  let pairDayData = PairDayData.load(dayPairID);
+  let timestamp = event.block.timestamp.toI32()
+  let dayID = timestamp / 86400
+  let dayStartTimestamp = dayID * 86400
+  let dayPairID = event.address
+    .toHexString()
+    .concat('-')
+    .concat(BigInt.fromI32(dayID).toString())
+  let pair = Pair.load(event.address.toHexString())
+  let pairDayData = PairDayData.load(dayPairID)
   if (pairDayData === null) {
-    pairDayData = new PairDayData(dayPairID);
-    pairDayData.date = dayStartTimestamp;
-    pairDayData.token0 = pair.token0;
-    pairDayData.token1 = pair.token1;
-    pairDayData.pairAddress = event.address;
-    pairDayData.dailyVolumeToken0 = ZERO_BD;
-    pairDayData.dailyVolumeToken1 = ZERO_BD;
-    pairDayData.dailyVolumeUSD = ZERO_BD;
-    pairDayData.dailyTxns = ZERO_BI;
+    pairDayData = new PairDayData(dayPairID)
+    pairDayData.date = dayStartTimestamp
+    pairDayData.token0 = pair.token0
+    pairDayData.token1 = pair.token1
+    pairDayData.pairAddress = event.address
+    pairDayData.dailyVolumeToken0 = ZERO_BD
+    pairDayData.dailyVolumeToken1 = ZERO_BD
+    pairDayData.dailyVolumeUSD = ZERO_BD
+    pairDayData.dailyTxns = ZERO_BI
   }
 
-  pairDayData.totalSupply = pair.totalSupply;
-  pairDayData.reserve0 = pair.reserve0;
-  pairDayData.reserve1 = pair.reserve1;
-  pairDayData.reserveUSD = pair.reserveUSD;
-  pairDayData.dailyTxns = pairDayData.dailyTxns.plus(ONE_BI);
-  pairDayData.save();
+  pairDayData.totalSupply = pair.totalSupply
+  pairDayData.reserve0 = pair.reserve0
+  pairDayData.reserve1 = pair.reserve1
+  pairDayData.reserveUSD = pair.reserveUSD
+  pairDayData.dailyTxns = pairDayData.dailyTxns.plus(ONE_BI)
+  pairDayData.save()
 
-  return pairDayData as PairDayData;
+  return pairDayData as PairDayData
 }
 
 export function updatePairHourData(event: ethereum.Event): PairHourData {
-  let timestamp = event.block.timestamp.toI32();
-  let hourIndex = timestamp / 3600; // get unique hour within unix history
-  let hourStartUnix = hourIndex * 3600; // want the rounded effect
-  let hourPairID = event.address.toHexString().concat("-").concat(BigInt.fromI32(hourIndex).toString());
-  let pair = Pair.load(event.address.toHexString());
-  let pairHourData = PairHourData.load(hourPairID);
+  let timestamp = event.block.timestamp.toI32()
+  let hourIndex = timestamp / 3600 // get unique hour within unix history
+  let hourStartUnix = hourIndex * 3600 // want the rounded effect
+  let hourPairID = event.address
+    .toHexString()
+    .concat('-')
+    .concat(BigInt.fromI32(hourIndex).toString())
+  let pair = Pair.load(event.address.toHexString())
+  let pairHourData = PairHourData.load(hourPairID)
   if (pairHourData === null) {
-    pairHourData = new PairHourData(hourPairID);
-    pairHourData.hourStartUnix = hourStartUnix;
-    pairHourData.pair = event.address.toHexString();
-    pairHourData.hourlyVolumeToken0 = ZERO_BD;
-    pairHourData.hourlyVolumeToken1 = ZERO_BD;
-    pairHourData.hourlyVolumeUSD = ZERO_BD;
-    pairHourData.hourlyTxns = ZERO_BI;
+    pairHourData = new PairHourData(hourPairID)
+    pairHourData.hourStartUnix = hourStartUnix
+    pairHourData.pair = event.address.toHexString()
+    pairHourData.hourlyVolumeToken0 = ZERO_BD
+    pairHourData.hourlyVolumeToken1 = ZERO_BD
+    pairHourData.hourlyVolumeUSD = ZERO_BD
+    pairHourData.hourlyTxns = ZERO_BI
   }
 
-  pairHourData.totalSupply = pair.totalSupply;
-  pairHourData.reserve0 = pair.reserve0;
-  pairHourData.reserve1 = pair.reserve1;
-  pairHourData.reserveUSD = pair.reserveUSD;
-  pairHourData.hourlyTxns = pairHourData.hourlyTxns.plus(ONE_BI);
-  pairHourData.save();
+  pairHourData.reserve0 = pair.reserve0
+  pairHourData.reserve1 = pair.reserve1
+  pairHourData.reserveUSD = pair.reserveUSD
+  pairHourData.hourlyTxns = pairHourData.hourlyTxns.plus(ONE_BI)
+  pairHourData.save()
 
-  return pairHourData as PairHourData;
+  return pairHourData as PairHourData
 }
 
 export function updateTokenDayData(token: Token, event: ethereum.Event): TokenDayData {
-  let bundle = Bundle.load("1");
-  let timestamp = event.block.timestamp.toI32();
-  let dayID = timestamp / 86400;
-  let dayStartTimestamp = dayID * 86400;
-  let tokenDayID = token.id.toString().concat("-").concat(BigInt.fromI32(dayID).toString());
+  let bundle = Bundle.load('1')
+  let timestamp = event.block.timestamp.toI32()
+  let dayID = timestamp / 86400
+  let dayStartTimestamp = dayID * 86400
+  let tokenDayID = token.id
+    .toString()
+    .concat('-')
+    .concat(BigInt.fromI32(dayID).toString())
 
-  let tokenDayData = TokenDayData.load(tokenDayID);
+  let tokenDayData = TokenDayData.load(tokenDayID)
   if (tokenDayData === null) {
-    tokenDayData = new TokenDayData(tokenDayID);
-    tokenDayData.date = dayStartTimestamp;
-    tokenDayData.token = token.id;
-    tokenDayData.priceUSD = token.derivedBNB.times(bundle.bnbPrice);
-    tokenDayData.dailyVolumeToken = ZERO_BD;
-    tokenDayData.dailyVolumeBNB = ZERO_BD;
-    tokenDayData.dailyVolumeUSD = ZERO_BD;
-    tokenDayData.dailyTxns = ZERO_BI;
-    tokenDayData.totalLiquidityUSD = ZERO_BD;
+    tokenDayData = new TokenDayData(tokenDayID)
+    tokenDayData.date = dayStartTimestamp
+    tokenDayData.token = token.id
+    tokenDayData.priceUSD = token.derivedBNB.times(bundle.bnbPrice)
+    tokenDayData.dailyVolumeToken = ZERO_BD
+    tokenDayData.dailyVolumeBNB = ZERO_BD
+    tokenDayData.dailyVolumeUSD = ZERO_BD
+    tokenDayData.dailyTxns = ZERO_BI
+    tokenDayData.totalLiquidityUSD = ZERO_BD
   }
-  tokenDayData.priceUSD = token.derivedBNB.times(bundle.bnbPrice);
-  tokenDayData.totalLiquidityToken = token.totalLiquidity;
-  tokenDayData.totalLiquidityBNB = token.totalLiquidity.times(token.derivedBNB as BigDecimal);
-  tokenDayData.totalLiquidityUSD = tokenDayData.totalLiquidityBNB.times(bundle.bnbPrice);
-  tokenDayData.dailyTxns = tokenDayData.dailyTxns.plus(ONE_BI);
-  tokenDayData.save();
+  tokenDayData.priceUSD = token.derivedBNB.times(bundle.bnbPrice)
+  tokenDayData.totalLiquidityToken = token.totalLiquidity
+  tokenDayData.totalLiquidityBNB = token.totalLiquidity.times(token.derivedBNB as BigDecimal)
+  tokenDayData.totalLiquidityUSD = tokenDayData.totalLiquidityBNB.times(bundle.bnbPrice)
+  tokenDayData.dailyTxns = tokenDayData.dailyTxns.plus(ONE_BI)
+  tokenDayData.save()
 
   /**
    * @todo test if this speeds up sync
@@ -116,5 +124,5 @@ export function updateTokenDayData(token: Token, event: ethereum.Event): TokenDa
   // updateStoredTokens(tokenDayData as TokenDayData, dayID)
   // updateStoredPairs(tokenDayData as TokenDayData, dayPairID)
 
-  return tokenDayData as TokenDayData;
+  return tokenDayData as TokenDayData
 }
--- src/exchange/pricing.ts
+++ src/exchange/pricing.ts
@@ -1,42 +1,40 @@
 /* eslint-disable prefer-const */
-import { BigDecimal, Address } from "@graphprotocol/graph-ts/index";
-import { Pair, Token, Bundle } from "../../generated/schema";
-import { ZERO_BD, factoryContract, ADDRESS_ZERO, ONE_BD } from "./utils";
+import { Pair, Token, Bundle } from '../../generated/schema'
+import { BigDecimal, Address, BigInt } from '@graphprotocol/graph-ts/index'
+import { ZERO_BD, factoryContract, ADDRESS_ZERO, ONE_BD } from './utils'
 
 const WBNB_ADDRESS = "0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c";
-const WBNB_BUSD_PAIR = "0x03386B91E1362cf5d790fbbf03388E5b94Abe50a"; // created block 6678884
-const DAI_WBNB_PAIR = "0x50d6e8E190D055A0A425d1B7094e98a0e6F0488d"; // created block 6678911
-const USDT_WBNB_PAIR = "0x424a3B8E9F7f1590826cC71745E1c7A3A8De8AeC"; // created block 6680913
+const WBNB_BUSD_PAIR = "0x03386b91e1362cf5d790fbbf03388e5b94abe50a"; // created block 6678884
+const DAI_WBNB_PAIR = "0x50d6e8e190d055a0a425d1b7094e98a0e6f0488d"; // created block 6678911
+const USDT_WBNB_PAIR = "0x424a3b8e9f7f1590826cc71745e1c7a3a8de8aec"; // created block 6680913
 
 export function getBnbPriceInUSD(): BigDecimal {
   // fetch bnb prices for each stablecoin
-  let usdtPair = Pair.load(USDT_WBNB_PAIR); // usdt is token0
-  let busdPair = Pair.load(WBNB_BUSD_PAIR); // busd is token1
-  let daiPair = Pair.load(DAI_WBNB_PAIR); // dai is token0
+  let usdtPair = Pair.load(USDT_WBNB_PAIR) // usdt is token0
+  let busdPair = Pair.load(WBNB_BUSD_PAIR) // busd is token1
+  let daiPair = Pair.load(DAI_WBNB_PAIR) // dai is token0
 
   // all 3 have been created
   if (daiPair !== null && busdPair !== null && usdtPair !== null) {
     let totalLiquidityBNB = daiPair.reserve1.plus(busdPair.reserve0).plus(usdtPair.reserve1);
-    let daiWeight = daiPair.reserve1.div(totalLiquidityBNB);
-    let busdWeight = busdPair.reserve0.div(totalLiquidityBNB);
-    let usdtWeight = usdtPair.reserve1.div(totalLiquidityBNB);
+    let daiWeight = daiPair.reserve1.div(totalLiquidityBNB)
+    let busdWeight = busdPair.reserve0.div(totalLiquidityBNB)
+    let usdtWeight = usdtPair.reserve1.div(totalLiquidityBNB)
     return daiPair.token0Price
       .times(daiWeight)
       .plus(busdPair.token1Price.times(busdWeight))
-      .plus(usdtPair.token0Price.times(usdtWeight));
+      .plus(usdtPair.token0Price.times(usdtWeight))
     // busd and usdt have been created
   } else if (busdPair !== null && usdtPair !== null) {
-    let totalLiquidityBNB = busdPair.reserve0.plus(usdtPair.reserve1);
-    let busdWeight = busdPair.reserve0.div(totalLiquidityBNB);
-    let usdtWeight = usdtPair.reserve1.div(totalLiquidityBNB);
-    return busdPair.token1Price.times(busdWeight).plus(usdtPair.token0Price.times(usdtWeight));
+    let totalLiquidityBNB = busdPair.reserve0.plus(usdtPair.reserve1)
+    let busdWeight = busdPair.reserve0.div(totalLiquidityBNB)
+    let usdtWeight = usdtPair.reserve1.div(totalLiquidityBNB)
+    return busdPair.token1Price.times(busdWeight).plus(usdtPair.token0Price.times(usdtWeight))
     // usdt is the only pair so far
   } else if (busdPair !== null) {
-    return busdPair.token1Price;
-  } else if (usdtPair !== null) {
-    return usdtPair.token0Price;
+    return busdPair.token1Price
   } else {
-    return ZERO_BD;
+    return ZERO_BD
   }
 }
 
@@ -52,10 +50,13 @@ let WHITELIST: string[] = [
   "0x7130d2a12b9bcbfae4f2634d864a1ee1ce3ead9c", // BTCB
   "0x2170ed0880ac9a755fd29b2688956bd959f933f8", // WETH
   "0x250632378e573c6be1ac2f97fcdf00515d0aa91b", // BETH
-];
+]
+
+// minimum liquidity required to count towards tracked volume for pairs with small # of Lps
+let MINIMUM_USD_THRESHOLD_NEW_PAIRS = BigDecimal.fromString('500')
 
 // minimum liquidity for price to get tracked
-let MINIMUM_LIQUIDITY_THRESHOLD_BNB = BigDecimal.fromString("5");
+let MINIMUM_LIQUIDITY_THRESHOLD_BNB = BigDecimal.fromString('1')
 
 /**
  * Search through graph to find derived BNB per token.
@@ -63,24 +64,24 @@ let MINIMUM_LIQUIDITY_THRESHOLD_BNB = BigDecimal.fromString("5");
  **/
 export function findBnbPerToken(token: Token): BigDecimal {
   if (token.id == WBNB_ADDRESS) {
-    return ONE_BD;
+    return ONE_BD
   }
   // loop through whitelist and check if paired with any
   for (let i = 0; i < WHITELIST.length; ++i) {
-    let pairAddress = factoryContract.getPair(Address.fromString(token.id), Address.fromString(WHITELIST[i]));
+    let pairAddress = factoryContract.getPair(Address.fromString(token.id), Address.fromString(WHITELIST[i]))
     if (pairAddress.toHexString() != ADDRESS_ZERO) {
-      let pair = Pair.load(pairAddress.toHexString());
+      let pair = Pair.load(pairAddress.toHexString())
       if (pair.token0 == token.id && pair.reserveBNB.gt(MINIMUM_LIQUIDITY_THRESHOLD_BNB)) {
-        let token1 = Token.load(pair.token1);
-        return pair.token1Price.times(token1.derivedBNB as BigDecimal); // return token1 per our token * BNB per token 1
+        let token1 = Token.load(pair.token1)
+        return pair.token1Price.times(token1.derivedBNB as BigDecimal) // return token1 per our token * BNB per token 1
       }
       if (pair.token1 == token.id && pair.reserveBNB.gt(MINIMUM_LIQUIDITY_THRESHOLD_BNB)) {
-        let token0 = Token.load(pair.token0);
-        return pair.token0Price.times(token0.derivedBNB as BigDecimal); // return token0 per our token * BNB per token 0
+        let token0 = Token.load(pair.token0)
+        return pair.token0Price.times(token0.derivedBNB as BigDecimal) // return token0 per our token * BNB per token 0
       }
     }
   }
-  return ZERO_BD; // nothing was found return 0
+  return ZERO_BD // nothing was found return 0
 }
 
 /**
@@ -90,32 +91,57 @@ export function findBnbPerToken(token: Token): BigDecimal {
  * If neither is, return 0
  */
 export function getTrackedVolumeUSD(
-  bundle: Bundle,
   tokenAmount0: BigDecimal,
   token0: Token,
   tokenAmount1: BigDecimal,
-  token1: Token
+  token1: Token,
+  pair: Pair
 ): BigDecimal {
-  let price0 = token0.derivedBNB.times(bundle.bnbPrice);
-  let price1 = token1.derivedBNB.times(bundle.bnbPrice);
+  let bundle = Bundle.load('1')
+  let price0 = token0.derivedBNB.times(bundle.bnbPrice)
+  let price1 = token1.derivedBNB.times(bundle.bnbPrice)
+
+  // if less than 5 LPs, require high minimum reserve amount amount or return 0
+  if (pair.liquidityProviderCount.lt(BigInt.fromI32(5))) {
+    let reserve0USD = pair.reserve0.times(price0)
+    let reserve1USD = pair.reserve1.times(price1)
+    if (WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
+      if (reserve0USD.plus(reserve1USD).lt(MINIMUM_USD_THRESHOLD_NEW_PAIRS)) {
+        return ZERO_BD
+      }
+    }
+    if (WHITELIST.includes(token0.id) && !WHITELIST.includes(token1.id)) {
+      if (reserve0USD.times(BigDecimal.fromString('2')).lt(MINIMUM_USD_THRESHOLD_NEW_PAIRS)) {
+        return ZERO_BD
+      }
+    }
+    if (!WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
+      if (reserve1USD.times(BigDecimal.fromString('2')).lt(MINIMUM_USD_THRESHOLD_NEW_PAIRS)) {
+        return ZERO_BD
+      }
+    }
+  }
 
   // both are whitelist tokens, take average of both amounts
   if (WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
-    return tokenAmount0.times(price0).plus(tokenAmount1.times(price1)).div(BigDecimal.fromString("2"));
+    return tokenAmount0
+      .times(price0)
+      .plus(tokenAmount1.times(price1))
+      .div(BigDecimal.fromString('2'))
   }
 
   // take full value of the whitelisted token amount
   if (WHITELIST.includes(token0.id) && !WHITELIST.includes(token1.id)) {
-    return tokenAmount0.times(price0);
+    return tokenAmount0.times(price0)
   }
 
   // take full value of the whitelisted token amount
   if (!WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
-    return tokenAmount1.times(price1);
+    return tokenAmount1.times(price1)
   }
 
   // neither token is on white list, tracked volume is 0
-  return ZERO_BD;
+  return ZERO_BD
 }
 
 /**
@@ -125,30 +151,30 @@ export function getTrackedVolumeUSD(
  * If neither is, return 0
  */
 export function getTrackedLiquidityUSD(
-  bundle: Bundle,
   tokenAmount0: BigDecimal,
   token0: Token,
   tokenAmount1: BigDecimal,
   token1: Token
 ): BigDecimal {
-  let price0 = token0.derivedBNB.times(bundle.bnbPrice);
-  let price1 = token1.derivedBNB.times(bundle.bnbPrice);
+  let bundle = Bundle.load('1')
+  let price0 = token0.derivedBNB.times(bundle.bnbPrice)
+  let price1 = token1.derivedBNB.times(bundle.bnbPrice)
 
   // both are whitelist tokens, take average of both amounts
   if (WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
-    return tokenAmount0.times(price0).plus(tokenAmount1.times(price1));
+    return tokenAmount0.times(price0).plus(tokenAmount1.times(price1))
   }
 
   // take double value of the whitelisted token amount
   if (WHITELIST.includes(token0.id) && !WHITELIST.includes(token1.id)) {
-    return tokenAmount0.times(price0).times(BigDecimal.fromString("2"));
+    return tokenAmount0.times(price0).times(BigDecimal.fromString('2'))
   }
 
   // take double value of the whitelisted token amount
   if (!WHITELIST.includes(token0.id) && WHITELIST.includes(token1.id)) {
-    return tokenAmount1.times(price1).times(BigDecimal.fromString("2"));
+    return tokenAmount1.times(price1).times(BigDecimal.fromString('2'))
   }
 
   // neither token is on white list, tracked volume is 0
-  return ZERO_BD;
+  return ZERO_BD
 }
